06eb7d5ba3a473bba0cc3aa2d9a31e235870a7d7
Tchouamou Eric herve <tchouamou@gmail.com>
2019-12-30 02:21:36 +0100

    Add files via upload

---
+++ class_phpexplorator.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * class_phpexplorator.php
+ * ---------
+ * begin    : July 2004 - October 2011
+ * copyright   : Camaroes Ver 3.03 (C) 2004-2011 T.E.H
+ * www     : http://sourceforge.net/projects/camaroes/
+ */
+
+/*  @license http://www.gnu.org/copyleft/gpl.html GNU/GPL */
+/*
+Copyright (c) 2011, Tchouamou Eric Herve  <tchouamou@gmail.com>
+All rights reserved.
+
+
+
+
+
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMIT TO, THE IMPLIED
+
+BUT NOT LIMIT TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+
+
+
+
+
+
+
+
+class_phpexplorator.php, Ver 3.03 
+*/
+
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+include_once($cmr->get_path("class") . "class/phpexplorator.class.php");
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+?>
